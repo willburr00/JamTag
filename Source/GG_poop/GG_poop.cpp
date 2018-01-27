9a65e1d92f3d5413f558381cf3626b4df990d85b
cbrunel <constant.95@gmail.com>
2018-01-27 11:39:23 -0500

    4 Characters with 4 controllers

---
+++ Source/GG_poop/GG_poop.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "GG_poop.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, GG_poop, "GG_poop" );
---
+++ Source/GG_poop/Player/Character/PlayableCharacter.cpp
@@ -0,0 +1,47 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "PlayableCharacter.h"
+
+
+// Sets default values
+APlayableCharacter::APlayableCharacter()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void APlayableCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void APlayableCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	float fForwardValue = InputComponent->GetAxisValue(TEXT("MoveForward"));
+	float fRightValue = InputComponent->GetAxisValue(TEXT("MoveRight"));
+
+	FVector movingDirection(fForwardValue, fRightValue, 0);
+	movingDirection.Normalize();
+
+	AddMovementInput(movingDirection);
+}
+
+// Called to bind functionality to input
+void APlayableCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	PlayerInputComponent->BindAxis(TEXT("MoveForward"));
+	PlayerInputComponent->BindAxis(TEXT("MoveRight"));
+
+	if (GEngine)
+	{
+		GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Yellow, "Player controller Axis attached on " + GetName());
+	}
+}
